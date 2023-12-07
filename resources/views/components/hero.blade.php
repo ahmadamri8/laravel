3f7abe82ce578579b3a766b6e1224d45b7356bf5
Amri <ahmadamri0809@gmail.com>
2023-12-08 06:43:40 +0700

    first commit

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,7 @@
+<div class="p-5 mb-4 text-bg-dark">
+  <div class="container-fluid py-5">
+    <h1 class="display-5 fw-bold">Dompet Anda adalah Sahabat Terbaik Kami</h1>
+    <h3>Datang ke sini untuk : </h3>
+    <p class="col-md-8 fs-4">Fashion Terjangkau, Harga Tak Terkalahkan.</p>
+  </div>
+</div>
